34b147449d694bedc5d6d40ec326ce6af9b7dbb8
Sebb767 <sebb@sebb767.de>
2017-03-22 16:40:59 +0100

    Added Array generator

---
+++ src/main/java/de/sebb767/pvs/helper/NumberGenerator.java
@@ -0,0 +1,46 @@
+package de.sebb767.pvs.helper;
+
+import java.util.Random;
+
+/**
+ * Created by proj on 3/22/17.
+ */
+public class NumberGenerator {
+    public class ArrayContainer {
+        protected Integer[] data;
+        protected long sum;
+        protected double average;
+
+        public ArrayContainer(Integer[] data, long sum, double average) {
+            this.data = data;
+            this.sum = sum;
+            this.average = average;
+        }
+
+        public Integer[] getData() {
+            return data;
+        }
+
+        public long getSum() {
+            return sum;
+        }
+
+        public double getAverage() {
+            return average;
+        }
+    }
+
+    public ArrayContainer generateRandomArray(int size) {
+        Integer[] data = new Integer[size];
+        long sum = 0;
+
+        Random rng = new Random();
+
+        for (int i = 0; i < size; i++) {
+            data[i] = rng.nextInt();
+            sum += data[i];
+        }
+
+        return new ArrayContainer(data, sum, ((double)sum) / size);
+    }
+}
