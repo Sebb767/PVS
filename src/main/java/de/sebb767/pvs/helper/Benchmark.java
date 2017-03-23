412c5edfc207071d547c4b3d69dcf5983761affc
Sebb767 <sebb@sebb767.de>
2017-03-23 11:49:58 +0100

    Added optional reset function, called after each benchmark run, to Benchmark class

--- src/main/java/de/sebb767/pvs/helper/Benchmark.java
+++ src/main/java/de/sebb767/pvs/helper/Benchmark.java
@@ -8,6 +8,11 @@ public class Benchmark {
 
     public void run(Runnable func, String name)
     {
+        this.run(func, "Benchmark", null);
+    }
+
+    public void run(Runnable func, String name, Runnable resetFn)
+    {
         System.out.print("Starting benchmark ... ");
 
         long endTime = 0;
@@ -16,6 +21,9 @@ public class Benchmark {
         func.run();
 
         endTime = System.currentTimeMillis();
+
+        if(resetFn != null)
+            resetFn.run();
 
         System.out.printf("done. %s required %d ms.\n", name, endTime - startTime);
     }
