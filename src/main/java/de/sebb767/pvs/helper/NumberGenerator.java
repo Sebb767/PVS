ac2bb2384ba561556d2b7cb15a97edccba765bbf
Sebb767 <sebb@sebb767.de>
2017-04-04 09:06:19 +0200

    Added Cache to NumberGenerator

--- src/main/java/de/sebb767/pvs/helper/NumberGenerator.java
+++ src/main/java/de/sebb767/pvs/helper/NumberGenerator.java
@@ -1,20 +1,28 @@
 package de.sebb767.pvs.helper;
 
+import com.google.gson.Gson;
+
+import java.io.*;
 import java.util.Random;
 
 /**
  * Created by proj on 3/22/17.
  */
 public class NumberGenerator {
-    public class ArrayContainer {
+    public class ArrayContainer implements Serializable {
         protected Integer[] data;
         protected long sum;
         protected double average;
+        protected Integer searchedElement;
+
+        transient protected boolean fromCache = false;
 
         public ArrayContainer(Integer[] data, long sum, double average) {
             this.data = data;
             this.sum = sum;
             this.average = average;
+
+            searchedElement = data[(int)(Math.random() * data.length)];
         }
 
         public Integer[] getData() {
@@ -23,6 +31,14 @@ public class NumberGenerator {
 
         public long getSum() {
             return sum;
+        }
+
+        public Integer getSearchedElement() {
+            return searchedElement;
+        }
+
+        public boolean isFromCache() {
+            return fromCache;
         }
 
         public double getAverage() {
@@ -34,7 +50,24 @@ public class NumberGenerator {
         return generateRandomArray(size, 0xFFFFFFFF);
     }
 
-    public ArrayContainer generateRandomArray(int size, int mask) {
+    public ArrayContainer generateRandomArray(int size, int mask) { return generateRandomArray(size, mask, true); }
+
+    public ArrayContainer generateRandomArray(int size, int mask, boolean useCached) {
+
+        String cachePath = "/tmp/arraycache_s" + size + "_m" + mask + ".json";
+        Gson gs = new Gson();
+
+        if(useCached)
+        {
+            try {
+                ArrayContainer ac = gs.fromJson(new FileReader(cachePath), ArrayContainer.class);
+                return ac;
+            } catch (FileNotFoundException e) {
+                // nothing cached
+            }
+        }
+
+
         Integer[] data = new Integer[size];
         long sum = 0;
 
@@ -45,6 +78,16 @@ public class NumberGenerator {
             sum += data[i];
         }
 
-        return new ArrayContainer(data, sum, ((double)sum) / size);
+        ArrayContainer ac = new ArrayContainer(data, sum, ((double)sum) / size);
+
+        if(useCached){
+            try {
+                gs.toJson(ac, new FileWriter(cachePath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return ac;
     }
 }
